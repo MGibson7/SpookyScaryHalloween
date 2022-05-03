5038a2614c85af96ac58c700ca8d26b160d1cbbf
MGibson7 <MGibsonASC@gmail.com>
2022-05-03 09:00:04 -0400

    changed background

---
+++ validation.js
@@ -0,0 +1,5 @@
+let pumpkin = document.getElementById("firstChar");
+pumpkin.addEventListener("click", event=>{
+    let siteGuide = document.getElementById("siteGuide");
+    siteGuide.innerHTML = '<img id = "pumpkin" src="./img/pumpkinChar.webp" alt="pumpkin">a';
+})
