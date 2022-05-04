4ca6d28b1d00e1d98cdac489add8ddd8c44bc0ec
MGibson7 <MGibsonASC@gmail.com>
2022-05-04 14:36:22 -0400

    new functionality random images displayed for links

--- validation.js
+++ validation.js
@@ -35,25 +35,39 @@ thirdChar.addEventListener('click', event =>{
 
 
 
- //   const backgroundPositions = [
-//        "randomImage1",
- //       "randomImage2",
- ////       "randomImage3"
+ const backgroundPositions = [
+            "startingPoint",
+             "aboutUs",
+            "tickets",
+            "merchandise",
+            "reviews",
+            "photos"
 
- //   ]
+  ]
 
- //   function randomImage() {
-  //      const randomImages = [
-  //          "./img/hall1.jpg", 
-   //         "./img/hall1.jpg", 
-   //         "./img/hall1.jpg"
-   //     ],
-   //     randomIndex = Math.floor(Math.random() * randomImages.length),
-    //    backgroundImage = randomImages[randomIndex],
-    //    randomIndex2 = Math.floor(Math.random() * backgroundPositions.length);
+function randomImage() {
+    const randomImages = [
+             "./img/mummy.png",     
+             "./img/reaper.png", 
+            "./img/cat.png",
+            "./img/flytrap.png",
+            "./img/spider.png",
+            "./img/evilpump.png"
+
+
+   ],
+          randomIndex = Math.floor(Math.random() * randomImages.length),
+          backgroundImage = randomImages[randomIndex],
+          randomIndex2 = Math.floor(Math.random() * backgroundPositions.length);
     
-     //   document.getElementById(`${backgroundPositions[randomIndex2]}`).innerHTML = `<img src = ${backgroundImage}>`;
-    //}
+          
+          for (background in backgroundPositions) {
+            let current = document.getElementById(`${backgroundPositions[background]}`).innerText
+            document.getElementById(`${backgroundPositions[background]}`).innerHTML = `${current} <img src = ${backgroundImage}>`;
+
+          }
+          
+    }
     
-    //randomImage();
-    //setInterval(randomImage, 1000);
+    randomImage();
+    setInterval(randomImage, 1000);
